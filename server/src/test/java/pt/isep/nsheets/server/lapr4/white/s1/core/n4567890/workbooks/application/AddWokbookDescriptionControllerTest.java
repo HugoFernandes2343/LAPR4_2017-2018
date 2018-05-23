e9976657d2730432f581f057ee082889c30b690e
Alexandre Braganca <atb@isep.ipp.pt>
2018-05-23 08:39:28 +0100

    Added persistence.xml to server tests.

--- server/src/test/java/pt/isep/nsheets/server/lapr4/white/s1/core/n4567890/workbooks/application/AddWokbookDescriptionControllerTest.java
+++ server/src/test/java/pt/isep/nsheets/server/lapr4/white/s1/core/n4567890/workbooks/application/AddWokbookDescriptionControllerTest.java
@@ -28,11 +28,11 @@ public class AddWokbookDescriptionControllerTest {
 		Properties props = new Properties();
 		props.put("persistence.repositoryFactory",
 				"pt.isep.nsheets.server.lapr4.white.s1.core.n4567890.workbooks.persistence.jpa.JpaRepositoryFactory");
-		props.put("persistence.persistenceUnit", "lapr4.NSheetsPU");
+		props.put("persistence.persistenceUnit", "lapr4.NSheetsPU-test");
 
 		// Other JPA properties that one might want to override from the ones in
 		// persistence.xml
-		props.put("javax.persistence.jdbc.url", "jdbc:h2:mem:");
+		// props.put("javax.persistence.jdbc.url", "jdbc:h2:mem:");
 		props.put("javax.persistence.schema-generation.database.action", "create");
 		// appProps.put("javax.persistence.jdbc.password", "");
 		// appProps.put("javax.persistence.jdbc.driver", "org.h2.Driver");
