abc392f60a2c82f01d7909b9e59e979bc606c5d7
jsant <jsant@LAPTOP-O5VNMES8>
2018-06-01 19:53:32 +0100

    UI export to csv [Implementation][Issue #37]

--- nsheets/src/main/java/pt/isep/nsheets/client/lapr4/green/s1/s1150575/application/settings/SettingsView.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/lapr4/green/s1/s1150575/application/settings/SettingsView.java
@@ -1,14 +1,37 @@
 package pt.isep.nsheets.client.lapr4.green.s1.s1150575.application.settings;
 
+import com.google.gwt.event.logical.shared.SelectionEvent;
 import javax.inject.Inject;
 
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
 import gwt.material.design.client.ui.MaterialButton;
+import gwt.material.design.client.ui.MaterialCheckBox;
+import gwt.material.design.client.ui.MaterialDropDown;
+import gwt.material.design.client.ui.MaterialLink;
+import gwt.material.design.client.ui.MaterialListBox;
 import gwt.material.design.client.ui.MaterialTextBox;
+import gwt.material.design.client.ui.MaterialToast;
 
 class SettingsView extends ViewImpl implements SettingsPresenter.MyView {
+
+    @UiField
+    MaterialButton btnCSVApply;
+
+    @UiField
+    MaterialCheckBox comma;
+
+    @UiField
+    MaterialCheckBox pointComma;
+
+    @UiField
+    MaterialCheckBox barra;
+
+    @UiField
+    MaterialCheckBox point;
 
     @Override
     public MaterialTextBox getWorkbookBox() {
@@ -27,14 +50,39 @@ class SettingsView extends ViewImpl implements SettingsPresenter.MyView {
 
     @Override
     public MaterialButton getApplyButton() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return btnCSVApply;
     }
-	interface Binder extends UiBinder<Widget, SettingsView> {
-	}
 
-	@Inject
-	SettingsView(Binder uiBinder) {
-		initWidget(uiBinder.createAndBindUi(this));
-	}
+    @Override
+    public MaterialCheckBox getComma() {
+        return comma;
+    }
+
+    @Override
+    public MaterialCheckBox getPointComma() {
+        return pointComma;
+    }
+
+    @Override
+    public MaterialCheckBox getBarra() {
+        return barra;
+    }
+
+    @Override
+    public MaterialCheckBox getPoint() {
+        return point;
+    }
+
+//    @Override
+//    public MaterialDropDown getDropButton() {
+//        return btnDrop;
+//    }
+    interface Binder extends UiBinder<Widget, SettingsView> {
+    }
+
+    @Inject
+    SettingsView(Binder uiBinder) {
+        initWidget(uiBinder.createAndBindUi(this));
+    }
 
 }
