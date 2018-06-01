1ad7b03945b4c1aae89e4df4ce954b7e3aceff9e
Paulo Jorge <Paulo Jorge@DESKTOP-7H7GA7I.lan>
2018-06-01 10:39:01 +0100

    IPC01.1 - User Authentication connection to the database

--- nsheets/src/main/java/pt/isep/nsheets/client/lapr4/green/s1/s1160570/application/login/LoginPresenter.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/lapr4/green/s1/s1160570/application/login/LoginPresenter.java
@@ -1,6 +1,7 @@
 package pt.isep.nsheets.client.lapr4.green.s1.s1160570.application.login;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.inject.Inject;
@@ -10,6 +11,8 @@ import com.gwtplatform.mvp.client.View;
 import com.gwtplatform.mvp.client.annotations.ProxyStandard;
 import com.gwtplatform.mvp.client.proxy.ProxyPlace;
 import com.gwtplatform.mvp.client.annotations.NameToken;
+import com.gwtplatform.mvp.client.annotations.NoGatekeeper;
+import com.gwtplatform.mvp.shared.proxy.PlaceRequest;
 import gwt.material.design.client.ui.MaterialToast;
 import java.util.ArrayList;
 import pt.isep.nsheets.client.application.ApplicationPresenter;
@@ -17,7 +20,7 @@ import pt.isep.nsheets.client.place.NameTokens;
 import pt.isep.nsheets.shared.services.UserDTO;
 import pt.isep.nsheets.shared.services.UsersService;
 import pt.isep.nsheets.shared.services.UsersServiceAsync;
-import pt.isep.nsheets.shared.services.WorkbookDescriptionDTO;
+
 
 public class LoginPresenter extends Presenter<LoginPresenter.MyView, LoginPresenter.MyProxy> {
 
@@ -30,6 +33,7 @@ public class LoginPresenter extends Presenter<LoginPresenter.MyView, LoginPresen
 
     @NameToken(NameTokens.login)
     @ProxyStandard
+    @NoGatekeeper
     interface MyProxy extends ProxyPlace<LoginPresenter> {
     }
 
@@ -38,11 +42,12 @@ public class LoginPresenter extends Presenter<LoginPresenter.MyView, LoginPresen
         super(eventBus, view, proxy, ApplicationPresenter.SLOT_CONTENT);
         this.view = view;
 
-        this.view.addClickHandler((event) -> {
+        this.view.addClickHandler((ClickEvent event) -> {
             UsersServiceAsync usersSvc = GWT.create(UsersService.class);
 
-            AsyncCallback<ArrayList<UserDTO>> callback = new AsyncCallback<ArrayList<UserDTO>>() {
-
+            AsyncCallback<ArrayList<UserDTO>> callback;
+            callback = new AsyncCallback<ArrayList<UserDTO>>() {
+                
                 @Override
                 public void onSuccess(ArrayList<UserDTO> result) {
                     MaterialToast.fireToast("Sucess");
@@ -50,7 +55,7 @@ public class LoginPresenter extends Presenter<LoginPresenter.MyView, LoginPresen
 
                 @Override
                 public void onFailure(Throwable caught) {
-                    MaterialToast.fireToast("Insucess");
+                    MaterialToast.fireToast("ALla");
                 }
             };
             usersSvc.getUsers(callback);
