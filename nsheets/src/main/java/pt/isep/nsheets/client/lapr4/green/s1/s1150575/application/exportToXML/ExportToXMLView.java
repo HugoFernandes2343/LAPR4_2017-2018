090b6eb112ab81f7de7b30b3bd7aeae3eb98b64c
João <1150575@isep.ipp.pt>
2018-06-04 14:25:02 +0100

    UI changes done. [Implementation][Working on issue #34]

---
+++ nsheets/src/main/java/pt/isep/nsheets/client/lapr4/green/s1/s1150575/application/exportToXML/ExportToXMLView.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.isep.nsheets.client.lapr4.green.s1.s1150575.application.exportToXML;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import gwt.material.design.addins.client.window.MaterialWindow;
+import gwt.material.design.client.constants.ButtonSize;
+import gwt.material.design.client.constants.TextAlign;
+import gwt.material.design.client.constants.WavesType;
+import gwt.material.design.client.ui.MaterialButton;
+import gwt.material.design.client.ui.MaterialLabel;
+import gwt.material.design.client.ui.MaterialPanel;
+import gwt.material.design.client.ui.MaterialRadioButton;
+import gwt.material.design.client.ui.MaterialTextBox;
+
+/**
+ *
+ * @author João Vieira
+ */
+public class ExportToXMLView extends Composite {
+
+    @UiField
+    MaterialWindow window;
+    @UiField
+    MaterialTextBox textBox1;
+    @UiField
+    MaterialTextBox textBox2;
+    @UiField
+    MaterialTextBox textBox3;
+    @UiField
+    MaterialTextBox textBox4;
+    @UiField
+    MaterialRadioButton radioButtonWorkbook;
+    @UiField
+    MaterialRadioButton radioButtonWorksheet;
+    @UiField
+    MaterialRadioButton radioButtonPartOfWorksheet;
+    @UiField
+    MaterialButton btnPartFields;
+    @UiField
+    MaterialButton btnExport;
+
+    private static ExportToXMLUiBinder uiBinder = GWT.create(ExportToXMLUiBinder.class);
+
+    interface ExportToXMLUiBinder extends UiBinder<Widget, ExportToXMLView> {
+    }
+
+    public ExportToXMLView() {
+
+        initWidget(uiBinder.createAndBindUi(this));
+        window.setPadding(32);
+        window.setHeight("600px");
+        window.setTextAlign(TextAlign.LEFT);
+        window.setTitle("Export to XML");
+        MaterialWindow.setOverlay(true);
+        MaterialLabel label1 = new MaterialLabel("Please select what you wish to export.");
+        radioButtonWorkbook.setName("Export");
+        radioButtonWorksheet.setName("Export");
+        radioButtonPartOfWorksheet.setName("Export");
+        window.add(label1);
+        MaterialPanel p0 = new MaterialPanel();
+        MaterialPanel p1 = new MaterialPanel();
+        MaterialPanel p2 = new MaterialPanel();
+        p0.setTextAlign(TextAlign.LEFT);
+        p1.setTextAlign(TextAlign.LEFT);
+        p2.setTextAlign(TextAlign.LEFT);
+        p0.add(radioButtonWorkbook);
+        p1.add(radioButtonWorksheet);
+        p2.add(radioButtonPartOfWorksheet);
+        window.add(p0);
+        window.add(p1);
+        window.add(p2);
+        textBox1.setEnabled(false);
+        textBox2.setEnabled(false);
+        textBox3.setEnabled(false);
+        textBox4.setEnabled(false);
+        window.add(textBox1);
+        window.add(textBox2);
+        window.add(textBox3);
+        window.add(textBox4);
+        btnPartFields.setWaves(WavesType.LIGHT);
+        btnPartFields.setSize(ButtonSize.MEDIUM);
+        btnPartFields.setEnabled(false);
+        MaterialPanel p3 = new MaterialPanel();
+        p3.setTextAlign(TextAlign.LEFT);
+        p3.add(btnPartFields);
+        window.add(p3);
+        btnExport.setWaves(WavesType.LIGHT);
+        btnExport.setSize(ButtonSize.MEDIUM);
+        MaterialPanel p4 = new MaterialPanel();
+        p4.setTextAlign(TextAlign.RIGHT);
+        p4.add(btnExport);
+        window.add(p4);
+        radioButtonPartOfWorksheet.addClickHandler(event -> {
+            textBox1.setEnabled(true);
+            textBox2.setEnabled(true);
+            textBox3.setEnabled(true);
+            textBox4.setEnabled(true);
+            btnPartFields.setEnabled(true);
+        });
+
+        window.open();
+    }
+
+    public MaterialTextBox getTextBox1() {
+        return textBox1;
+    }
+
+    public MaterialTextBox getTextBox2() {
+        return textBox2;
+    }
+
+    public MaterialTextBox getTextBox3() {
+        return textBox3;
+    }
+
+    public MaterialTextBox getTextBox4() {
+        return textBox4;
+    }
+
+    public MaterialButton getBtnPartFields() {
+        return btnPartFields;
+    }
+
+    public MaterialButton getBtnExport() {
+        return btnExport;
+    }
+
+}
