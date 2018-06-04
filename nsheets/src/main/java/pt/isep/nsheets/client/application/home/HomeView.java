b3d0abb20f1f8790d6e8cf4faa5782d7a31d41bb
André Vieira Rodrigues <1151136@isep.ipp.pt>
2018-06-04 23:13:08 +0100

    ipc06.1

--- nsheets/src/main/java/pt/isep/nsheets/client/application/home/HomeView.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/application/home/HomeView.java
@@ -12,6 +12,7 @@ import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
+import gwt.material.design.addins.client.window.MaterialWindow;
 
 import gwt.material.design.client.constants.Color;
 import gwt.material.design.client.constants.IconPosition;
@@ -27,6 +28,7 @@ import gwt.material.design.client.ui.MaterialLink;
 import gwt.material.design.client.ui.MaterialRow;
 import gwt.material.design.client.ui.MaterialNavBar;
 import gwt.material.design.client.ui.MaterialSearch;
+import gwt.material.design.client.ui.MaterialToast;
 import pt.isep.nsheets.shared.core.Workbook;
 import pt.isep.nsheets.shared.services.WorkbookDescriptionDTO;
 
@@ -57,8 +59,7 @@ class HomeView extends ViewImpl implements HomePresenter.MyView {
     MaterialLabel workbookDescription;
     
     @UiField
-    MaterialCard card;
-            
+    MaterialCard card;            
 
     @Inject
     HomeView(Binder uiBinder) {
@@ -76,47 +77,6 @@ class HomeView extends ViewImpl implements HomePresenter.MyView {
             navBarSearch.setVisible(false);
         });
     }
-
-//    private MaterialCard createCard(Workbook wb) {
-//        MaterialCard card = new MaterialCard();
-//        card.setBackgroundColor(Color.BLUE_DARKEN_1);
-//
-//        MaterialCardContent cardContent = new MaterialCardContent();
-//        cardContent.setTextColor(Color.WHITE);
-//
-//        MaterialCardTitle cardTitle = new MaterialCardTitle();
-//        cardTitle.setText(wb.getName());
-//        cardTitle.setIconType(IconType.INSERT_DRIVE_FILE);
-//        cardTitle.setIconPosition(IconPosition.RIGHT);
-//
-//        MaterialLabel label = new MaterialLabel();
-//        label.setText(wb.getDescription());
-//
-//        MaterialCardAction cardAction = new MaterialCardAction();
-//
-//        MaterialLink renameLink = new MaterialLink();
-//        renameLink.setText("Rename");
-//        renameLink.setIconType(IconType.EDIT);
-//        renameLink.setIconColor(Color.INDIGO);
-//        renameLink.setTextColor(Color.WHITE);
-//
-//        MaterialLink deleteLink = new MaterialLink();
-//        deleteLink.setText("Delete");
-//        deleteLink.setIconType(IconType.DELETE);
-//        deleteLink.setIconColor(Color.GREY);
-//        deleteLink.setTextColor(Color.WHITE);
-//
-//        cardContent.add(cardTitle);
-//        cardContent.add(label);
-//
-//        cardAction.add(renameLink);
-//        cardAction.add(deleteLink);
-//
-//        card.add(cardContent);
-//        card.add(cardAction);
-//
-//        return card;
-//    }
     
     @Override
     public void setContents(ArrayList<Workbook> contents) {
