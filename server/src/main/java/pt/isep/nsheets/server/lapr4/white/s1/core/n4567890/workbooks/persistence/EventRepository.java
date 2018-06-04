93b788555b13327835d47ac05121c3ab1b33fc87
Filipe <1161213@isep.ipp.pt>
2018-06-04 18:11:36 +0100

    [Core 6.1] All necessary controller created and need to be completed. Unused methods removem from EventRepository and added necessary methods

--- server/src/main/java/pt/isep/nsheets/server/lapr4/white/s1/core/n4567890/workbooks/persistence/EventRepository.java
+++ server/src/main/java/pt/isep/nsheets/server/lapr4/white/s1/core/n4567890/workbooks/persistence/EventRepository.java
@@ -1,16 +1,12 @@
 package pt.isep.nsheets.server.lapr4.white.s1.core.n4567890.workbooks.persistence;
 
 import eapli.framework.persistence.repositories.Repository;
-import pt.isep.nsheets.server.lapr4.green.s1.core.n1160570.login.domain.User;
 import pt.isep.nsheets.server.lapr4.red.s1.core.n1161213.events.domain.Event;
 
-import java.util.Date;
 
 public interface EventRepository extends Repository<Event, Long> {
 
-    public Iterable<Event> findAllByUser(User user);
+    public void deleteEvent(Long id);
 
-    public Iterable<Event> findUserEventsInDate(Date date);
-
-
+    public Event findById(Long id);
 }
--- server/src/main/java/pt/isep/nsheets/server/lapr4/white/s1/core/n4567890/workbooks/persistence/jpa/JpaEventRepository.java
+++ server/src/main/java/pt/isep/nsheets/server/lapr4/white/s1/core/n4567890/workbooks/persistence/jpa/JpaEventRepository.java
@@ -1,5 +1,6 @@
 package pt.isep.nsheets.server.lapr4.white.s1.core.n4567890.workbooks.persistence.jpa;
 
+import eapli.framework.persistence.repositories.impl.inmemory.NotFoundException;
 import pt.isep.nsheets.server.lapr4.green.s1.core.n1160570.login.domain.User;
 import pt.isep.nsheets.server.lapr4.red.s1.core.n1161213.events.domain.Event;
 import pt.isep.nsheets.server.lapr4.white.s1.core.n4567890.workbooks.persistence.EventRepository;
@@ -8,6 +9,7 @@ import pt.isep.nsheets.server.lapr4.white.s1.core.n4567890.workbooks.persistence
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Optional;
 
 public class JpaEventRepository extends NSheetsJpaRepositoryBase<Event, Long> implements EventRepository {
 
@@ -15,19 +17,13 @@ public class JpaEventRepository extends NSheetsJpaRepositoryBase<Event, Long> im
         super(settings);
     }
 
-    @Override
-    public Iterable<Event> findAllByUser(User user) {
-        HashMap<String, Object> params = new HashMap<>();
-        params.put("u", user);
-        Iterable<Event> ret = match("e.user =: u", params);
-        return ret;
+    public void deleteEvent(Long id){
+        super.deleteByPK(id);
     }
 
-    @Override
-    public Iterable<Event> findUserEventsInDate(Date date) {
-        HashMap<String, Object> params = new HashMap<>();
-        params.put("date", date);
-        Iterable<Event> ret = match("e.timestamp =: date", params);
-        return ret;
+    public Event findById(Long id){
+        Optional<Event> e = super.findOne(id);
+
+        return e.get();
     }
 }
