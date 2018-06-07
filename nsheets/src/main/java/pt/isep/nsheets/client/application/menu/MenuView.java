ebf973cc461c4b904770ece2a6a0fa5b71d75639
LAPTOP-HL75L5M8\David <1161109@isep.ipp.pt>
2018-06-07 15:50:21 +0100

    ref issue #26  Logout Button and methods added |  Email verification added

--- nsheets/src/main/java/pt/isep/nsheets/client/application/menu/MenuView.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/application/menu/MenuView.java
@@ -1,11 +1,13 @@
 package pt.isep.nsheets.client.application.menu;
 
+import com.google.gwt.event.dom.client.ClickHandler;
 import javax.inject.Inject;
 
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewWithUiHandlers;
+import gwt.material.design.client.ui.MaterialLink;
 
 import gwt.material.design.client.ui.MaterialNavBar;
 import gwt.material.design.client.ui.MaterialSideNavPush;
@@ -19,9 +21,18 @@ class MenuView extends ViewWithUiHandlers<MenuUiHandlers> implements MenuPresent
 	MaterialNavBar navBar;
 	@UiField
 	MaterialSideNavPush sideNav;
+        @UiField
+        MaterialLink btnLogout; 
 
 	@Inject
 	MenuView(Binder uiBinder) {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
+
+       @Override
+        public void addClickHandler(ClickHandler ch) {
+            btnLogout.addClickHandler(ch);
+        }
+        
+        
 }
