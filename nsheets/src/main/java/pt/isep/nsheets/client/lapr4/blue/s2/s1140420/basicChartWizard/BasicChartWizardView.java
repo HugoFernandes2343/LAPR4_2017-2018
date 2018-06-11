2d077fc2248fea3697efc98006a14865fd175a2f
RodrigoSoares <1140420@isep.ipp.pt>
2018-06-11 18:53:24 +0100

    [Core04.1][fixing issue #10][Implementation] - Fixes to actually generate the Bar Chart. Now I just need to capture the data from the range of cells, and it will at least be satisfactory

--- nsheets/src/main/java/pt/isep/nsheets/client/lapr4/blue/s2/s1140420/basicChartWizard/BasicChartWizardView.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/lapr4/blue/s2/s1140420/basicChartWizard/BasicChartWizardView.java
@@ -13,16 +13,28 @@ import pt.isep.nsheets.shared.core.Spreadsheet;
 public class BasicChartWizardView extends Composite{
 
     @UiField
-    MaterialWindow wizardWindowStep1, wizardWindowStep2;
+    MaterialWindow wizardWindowStep1;
 
     @UiField
-    MaterialTextBox chartName, upperCellInfo, lowerCellInfo;
+    MaterialWindow wizardWindowStep2;
+
+    @UiField
+    MaterialTextBox chartName;
+
+    @UiField
+    MaterialTextBox upperCellInfo;
+
+    @UiField
+    MaterialTextBox lowerCellInfo;
 
     @UiField
     MaterialListValueBox<String> linesOrColumnsBox;
 
     @UiField
     MaterialButton previewChartButton;
+
+    @UiField
+    MaterialBarChart chart;
 
     private static BasicChartWizardBinder uiBinder = GWT.create(BasicChartWizardBinder.class);
 
@@ -44,7 +56,7 @@ public class BasicChartWizardView extends Composite{
             String lowerCell = lowerCellInfo.getText();
             String upperCell = upperCellInfo.getText();
 
-
+            wizardWindowStep2.open();
         });
     }
 
