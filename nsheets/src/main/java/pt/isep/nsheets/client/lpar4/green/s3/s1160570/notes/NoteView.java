1d347974460b8306782bb99d615e2b21c67a00d3
Paulo Jorge <Paulo Jorge@DESKTOP-7H7GA7I>
2018-06-17 12:30:54 +0100

    ref issue #19 [Core07.1]-Create window to create note

--- nsheets/src/main/java/pt/isep/nsheets/client/lpar4/green/s3/s1160570/notes/NoteView.java
+++ nsheets/src/main/java/pt/isep/nsheets/client/lpar4/green/s3/s1160570/notes/NoteView.java
@@ -1,5 +1,6 @@
 package pt.isep.nsheets.client.lpar4.green.s3.s1160570.notes;
 
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Widget;
@@ -7,6 +8,7 @@ import javax.inject.Inject;
 
 import com.gwtplatform.mvp.client.ViewImpl;
 import gwt.material.design.addins.client.combobox.MaterialComboBox;
+import gwt.material.design.client.ui.MaterialButton;
 import gwt.material.design.client.ui.MaterialColumn;
 
 class NoteView extends ViewImpl implements NotePresenter.MyView {
@@ -17,6 +19,9 @@ class NoteView extends ViewImpl implements NotePresenter.MyView {
     @UiField
     MaterialColumn cards;
 
+    @UiField
+    MaterialButton newNoteButton;
+
     interface Binder extends UiBinder<Widget, NoteView> {
     }
 
@@ -24,4 +29,20 @@ class NoteView extends ViewImpl implements NotePresenter.MyView {
     NoteView(Binder uiBinder) {
         initWidget(uiBinder.createAndBindUi(this));
     }
+
+    @Override
+    public MaterialComboBox getComboBox() {
+        return comboBox;
+    }
+
+    @Override
+    public MaterialColumn getCards() {
+        return cards;
+    }
+
+    @Override
+    public void addClickHandler(ClickHandler ch) {
+        newNoteButton.addClickHandler(ch);
+    }
+
 }
